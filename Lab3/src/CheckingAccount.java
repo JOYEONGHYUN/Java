7ea5195a166063711f38578cabb640b730c66713
Avilrus <Avilrus@Avilrus-PC>
2017-03-23 11:03:35 +0900

    edit CheckingAccount

--- Lab3/src/CheckingAccount.java
+++ Lab3/src/CheckingAccount.java
@@ -5,35 +5,44 @@ class CheckingAccount extends Account{
 	private double interest;	 // 이자액     0.1
 	private double loan_interest;// 대출이자  0.7
 	
-	public CheckingAccount(double balance, String name, double credit_limit, double interest, double loan_interest) {
-		super(balance, name); // 부모에 있는것들을 전부 상속받아야됨
+	public CheckingAccount(double balance, double credit_limit, double interest, double loan_interest) {
+		super(balance); // 부모에 있는것들을 전부 상속받아야됨
 		
-		this.credit_limit = credit_limit;
-		this.interest = interest;
-		this.loan_interest = loan_interest;
-		setBalance(balance);
-		
+		this.credit_limit = credit_limit; // 한도액
+		this.interest = interest; // 이자
+		this.loan_interest = loan_interest; // 마이너스 통장 이자
+		setBalance(balance);		
 	};
 	
 	public void nextMonth()
 	{
 		if(getBalance()>0)
 		{
-			setBalance(getBalance()+getBalance()*interest);
+			setBalance( getBalance() + getBalance()*interest); // 한도금액이 0 보다 크면 0.1 이자 
 		}
 		else
 		{
-			setBalance(getBalance()+getBalance()*loan_interest);
+			setBalance( getBalance() + (getBalance()*loan_interest) ); // 한도금액이 마이너스이면 0.7 이자
 		}
 	}
 	
 	
 	@Override
-	public void debit(double balance)
+	public String debit(double balance)
 	{
+		if(credit_limit>getBalance()-balance)
+		{
+			return "Debit amount exceeded account balance";
+		}
+		else if ( (getBalance()-balance < 0) && ( credit_limit> (getBalance()-balance) ) )
+		{
+			return "Account' Balance < 0 ";
+		}
+		else
+		{
 		setBalance(getBalance()-balance);
+		return null;
+		}		
 	}
-	
-	
-	
+
 }
--- Lab3/src/CheckingAccountTest.java
+++ Lab3/src/CheckingAccountTest.java
@@ -6,28 +6,30 @@ public class CheckingAccountTest {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		
-		String ac1 = "account1";
-		String ac2 = "account2";
+		double debit;
+		double deposit;
+		String err;
 		
-		double debit2;
-		double deposit1;
-		
-		CheckingAccount account1 = new CheckingAccount(100.0, ac1,-100,0.01,0.07);  // 참조자 생성
-		CheckingAccount account2 = new CheckingAccount(100.0, ac2,-100,0.01,0.07);  // 참조자 생성
+		CheckingAccount account1 = new CheckingAccount(100.0,-100,0.01,0.07);  // 참조자 생성
+		CheckingAccount account2 = new CheckingAccount(100.0,-100,0.01,0.07);  // 참조자 생성
 		
 		
 		System.out.printf("Account1 balance %.2f\n",account1.getBalance()); // 계좌 내역 확인
 		System.out.printf("Account2 balance %.2f\n",account2.getBalance()); // 계좌 내역 확인
 		 // 계좌 내역 확인
 		System.out.print("Enter deposit amount for Account1 : ");
-		deposit1 = input.nextDouble(); // 예급 할 금액 입력
-		account1.credit(deposit1);
+		deposit = input.nextDouble(); // 예급 할 금액 입력
+		account1.credit(deposit);
 		
 		System.out.printf("Account1 balance %.2f\n",account1.getBalance()); // 계좌 내역 확인
 		System.out.printf("Account2 balance %.2f\n",account2.getBalance()); // 계좌 내역 확인
 		System.out.print("Enter withdrawal amount for account2 : ");
-		debit2 = input.nextDouble(); // 예급 할 금액 입력
-		account2.debit(debit2);
+		debit = input.nextDouble(); // 예급 할 금액 입력
+		err=account2.debit(debit);
+		if(err!=null)
+		{
+			System.out.println(err); // 한도초과
+		}
 		
 		System.out.printf("Account1 balance %.2f\n",account1.getBalance()); // 계좌 내역 확인
 		System.out.printf("Account2 balance %.2f\n",account2.getBalance()); // 계좌 내역 확인
@@ -39,5 +41,6 @@ public class CheckingAccountTest {
 		System.out.printf("Account1 balance %.2f\n",account1.getBalance()); // 계좌 내역 확인
 		System.out.printf("Account2 balance %.2f\n",account2.getBalance()); // 계좌 내역 확인
 		
+		input.close();
 	}
 }
